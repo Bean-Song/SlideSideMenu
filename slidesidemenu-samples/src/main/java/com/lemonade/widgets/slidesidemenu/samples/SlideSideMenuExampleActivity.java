d6873c2d1e83250ba74eb5af7c35099a47a03cf1
宋仕斌 <songshibin@weixing.gitlab.com>
2021-11-12 10:43:21 +0800

    clone from: https://github.com/lemonade-hq/SlideSideMenu.git 升级AndroidX

---
+++ slidesidemenu-samples/src/main/java/com/lemonade/widgets/slidesidemenu/samples/SlideSideMenuExampleActivity.java
@@ -0,0 +1,48 @@
+package com.lemonade.widgets.slidesidemenu.samples;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.lemonade.widgets.slidesidemenu.SlideSideMenuTransitionLayout;
+
+public class SlideSideMenuExampleActivity extends AppCompatActivity {
+
+    private SlideSideMenuTransitionLayout mSlideSideMenu;
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slide_side_menu_example);
+
+        // Grab the widget
+        mSlideSideMenu = (SlideSideMenuTransitionLayout)findViewById(R.id.slide_side_menu);
+
+        // Setup the toolbar
+        mToolbar = (Toolbar) findViewById(R.id.base_toolbar);
+        setSupportActionBar(mToolbar);
+        mToolbar.setTitleTextColor(Color.WHITE);
+
+        // Wire SideMenu with Toolbar
+        mToolbar.setNavigationIcon(R.drawable.ic_menu);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSlideSideMenu.toggle();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mSlideSideMenu != null && mSlideSideMenu.closeSideMenu()) {
+            // Closed the side menu, override the default back pressed behavior
+            return;
+        }
+        super.onBackPressed();
+    }
+}
